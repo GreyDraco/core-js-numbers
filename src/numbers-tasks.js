03a7e4fb47a17942fa1670ff4c4c24cd6d77109f
GreyDraco <ancientgreydragon@gmail.com>
2023-12-22 05:51:47 +0300

    feat: solve toNumber task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -219,8 +219,12 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const x = +value;
+  if (typeof x === typeof def && !Number.isNaN(x)) {
+    return +value;
+  }
+  return def;
 }
 
 /**
